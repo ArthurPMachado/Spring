4164ebfad4f5e31f8dd581c9930342012dc76555
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-15 02:19:13 -0300

    Feat: add ioc xml file and test it

--- InversaoDeControle/src/br/com/arthurmachado/ioc/MyApp.java
+++ InversaoDeControle/src/br/com/arthurmachado/ioc/MyApp.java
@@ -3,7 +3,7 @@ package br.com.arthurmachado.ioc;
 public class MyApp {
 
 	public static void main(String[] args) {
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		System.out.println(theCoach.getDailyWorkout());
 	}
