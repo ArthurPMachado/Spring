036c049ab2f357709d296e404b8d45d05a726d25
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-16 18:14:24 -0300

    Feat: inject literal values

--- InversaoDeControle/src/br/com/arthurmachado/iocdi/CricketCoach.java
+++ InversaoDeControle/src/br/com/arthurmachado/iocdi/CricketCoach.java
@@ -1,15 +1,17 @@
 package br.com.arthurmachado.iocdi;
 
 public class CricketCoach implements Coach {
-	
 	private FortuneService fortuneService;
+	
+	private String emailAddress;
+	private String team;
 	
 	public CricketCoach() {
 		System.out.println("Inside no-arg constructor");
 	}
 
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("Inside setter injection");
+		System.out.println("Inside setter injection - fortune");
 		this.fortuneService = fortuneService;
 	}
 
@@ -17,10 +19,29 @@ public class CricketCoach implements Coach {
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minutes";
 	}
-
+	
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
 
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside setter injection - email");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Inside setter injection - team");
+		this.team = team;
+	}
+
+	
 }
