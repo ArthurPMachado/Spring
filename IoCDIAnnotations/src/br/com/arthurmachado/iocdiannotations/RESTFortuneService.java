cc44f88875ef6b9b04d8905f7d57a46800054028
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-18 19:03:23 -0300

    Feat: add qualifier to tennisCoach

---
+++ IoCDIAnnotations/src/br/com/arthurmachado/iocdiannotations/RESTFortuneService.java
@@ -0,0 +1,14 @@
+package br.com.arthurmachado.iocdiannotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
