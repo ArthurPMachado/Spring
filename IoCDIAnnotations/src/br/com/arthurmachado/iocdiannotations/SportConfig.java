73b75950871f25603c999787b3f5f3213f6613ff
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-21 15:02:19 -0300

    Feat: add spring java config class

---
+++ IoCDIAnnotations/src/br/com/arthurmachado/iocdiannotations/SportConfig.java
@@ -0,0 +1,10 @@
+package br.com.arthurmachado.iocdiannotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.arthurmachado.iocdiannotations")
+public class SportConfig {
+
+}
