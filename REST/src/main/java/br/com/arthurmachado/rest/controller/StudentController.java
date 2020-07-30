2f77d3eb4384b3e7abf0327b3bd178a9527e3370
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-30 16:37:44 -0300

    Refactor: load data only once with @PostConstruct

--- REST/src/main/java/br/com/arthurmachado/rest/controller/StudentController.java
+++ REST/src/main/java/br/com/arthurmachado/rest/controller/StudentController.java
@@ -3,6 +3,8 @@ package br.com.arthurmachado.rest.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +14,23 @@ import br.com.arthurmachado.rest.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-
-	// Define endpoint for "/students" - return list of students
 	
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+	
+	// Define @PostConstruct to load the student data ... only once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
 		theStudents.add(new Student("Mary", "Smith"));
+	}
+
+	// Define endpoint for "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
 		
 		return theStudents;
 	}
