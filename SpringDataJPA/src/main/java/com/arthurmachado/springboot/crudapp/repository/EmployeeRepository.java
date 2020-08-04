46dbba14473fb449a039d414b7c0c2ee17be43f2
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-04 17:52:21 -0300

    Feat: add project to test JPARepository

---
+++ SpringDataJPA/src/main/java/com/arthurmachado/springboot/crudapp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.arthurmachado.springboot.crudapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arthurmachado.springboot.crudapp.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
