78411baa574f43a8bf1413081ea783d621572ce0
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-05 00:11:26 -0300

    Feat: test rest configuration, pagination and sorting

--- SpringDataREST/src/main/java/com/arthurmachado/springboot/crudapp/repository/EmployeeRepository.java
+++ SpringDataREST/src/main/java/com/arthurmachado/springboot/crudapp/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.arthurmachado.springboot.crudapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.arthurmachado.springboot.crudapp.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
